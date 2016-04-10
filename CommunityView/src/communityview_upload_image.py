f6dbb69dd41ddc1464e2d3bf3af74be7c55aa104
Jesper Jurcenoks <jesper@jurcenoks.com>
2016-04-10 15:06:19 -0700

    First attempt at server end for HTTP POST Image upload
    Upload files to Neighborhood Guard using HTTP instead of FTP or SFTP
    Installation: copy this file to communityview directory
    change attributes to allow execution like this:
    
    chmod +x communityview_upload_image.py

---
+++ CommunityView/src/communityview_upload_image.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+"""
+This script was adapted from http://code.activestate.com/recipes/273844-minimal-http-upload-cgi/
+with updates from http://stackoverflow.com/questions/12166158/upload-a-file-with-python 
+
+Adapted by Jesper Jurcenoks 2016
+Copyright Neighborhood Guard 2016
+License: GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+This script has security risks. A user could attempt to fill
+a disk partition with endless uploads. 
+If you have a system open to the public you would obviously want
+to limit the size and number of files written to the disk.
+"""
+import cgi
+import cgitb; cgitb.enable()
+import os, sys
+import shutil
+import re
+import localsettings
+
+HTML_TEMPLATE = """
+<html><head><title>File Upload</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body><h1>File Upload</h1>
+Filename: %s<br>
+%s<br>
+<form action="" method="POST" enctype="multipart/form-data">
+Date [yyyy-mm-dd] : <input type="text" name="date">
+Camera short name [a-z_0-9-]: <input type="text" name="camera">
+File name hh-mm-ss-nnnnnnn.jpg: <input name="file" type="file"><br>
+<input type="checkbox" value="true" name="status"> Status file<br>
+<input name="submit" type="submit">
+</form>
+</body>
+</html>"""
+
+def print_html_form (error, outpath):
+    """This prints out the html form. Note that the action is set to
+      the name of the script which makes this is a self-posting form.
+      In other words, this cgi both displays a form and processes it.
+    """
+    print "content-type: text/html\n"
+    print HTML_TEMPLATE % (outpath, error)
+
+    return
+    
+def save_uploaded_file ():
+    error = ""
+    outpath = ""
+    """This saves a file uploaded by an HTML form.
+       The form_field is the name of the file input field from the form.
+       For example, the following form_field would be "file_1":
+           <input name="file_1" type="file">
+       The upload_dir is the directory where the file will be written.
+       If no file was uploaded or if the field does not exist then
+       this does nothing.
+    """
+    
+    
+    form = cgi.FieldStorage()
+
+    if form.has_key("submit"):
+        if not form.has_key("file"):
+            error = "Error: form has no input 'file'"
+        else :
+            fileitem = form["file"]
+            if not fileitem.file or not fileitem.filename: 
+                error = "Error: file form fields is not type=file"
+            else:
+                if not form.has_key("camera"):
+                    error = "no camera submitted"
+                else:
+                    camerashortname_raw = form["camera"].value
+                    if camerashortname_raw <> None:
+                        matchobject=re.match("[\w-]*", camerashortname_raw)
+                        if matchobject==None or matchobject.group(0) == "" or len(camerashortname_raw) <> len(matchobject.group(0)):
+                            error = "Invalid Camera name"
+                        else:
+                            camerashortname = matchobject.group(0)
+                
+                            if form.has_key("status") and form["status"].value == "true" :
+                                outpath = os.path.join(localsettings.root, "status", camerashortname, fileitem.filename)
+                                with open(outpath, 'wb') as fout:
+                                    try:
+                                        shutil.copyfileobj(fileitem.file, fout, 100000)
+                                    except:
+                                        error = "unknown error writing file to disk"
+                            else:
+
+                                date_raw = form["date"].value
+                                if date_raw <> None:
+                                    matchobject=re.match("[\d-]*", date_raw)
+                                    if matchobject==None or matchobject.group(0) == "" or len(date_raw) <> len(matchobject.group(0)):
+                                        error = "Invalid date"
+                                    else:                           
+                                        image_date = matchobject.group(0)
+                                        outpath = os.path.join(localsettings.root,image_date, camerashortname, fileitem.filename)
+                                        
+                                        with open(outpath, 'wb') as fout:
+                                            try:
+                                                shutil.copyfileobj(fileitem.file, fout, 100000)
+                                            except:
+                                                error = "unknown error writing file to disk"
+    return (error, outpath)
+    
+(error, outpath) = save_uploaded_file ()
+
+print_html_form (error, outpath)
